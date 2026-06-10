72275e7cea7aba9ac4509e2ef1e3db832ff3a52e
prasad <prasadpawar252@gmail.com>
2026-06-10 12:58:36 +0530

    new chnages -13

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,17 +14,18 @@ module "ecr" {
 }
 
 module "ecs" {
-  source              = "./modules/ecs"
-  environment         = var.environment
-  image_uri           = var.image_uri
-  execution_role_arn  = module.iam.execution_role_arn
-  public_subnet_ids   = module.vpc.public_subnet_ids
+  source             = "./modules/ecs"
+  environment        = var.environment
+  image_uri          = var.image_uri
+  execution_role_arn = module.iam.execution_role_arn
+  public_subnet_ids  = module.vpc.public_subnet_ids
 }
 
 module "alb" {
-  source               = "./modules/alb"
-  environment          = var.environment
-  vpc_id               = module.vpc.vpc_id
-  public_subnet_ids    = module.vpc.public_subnet_ids
-  security_group_ids   = [] # TEMP (we will fix later)
+  source            = "./modules/alb"
+  environment       = var.environment
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_ids = module.vpc.public_subnet_ids
+
+  security_group_ids = [] # you can improve later
 }
--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -1,29 +1,3 @@
-resource "aws_ecs_cluster" "this" {
-  name = "${var.environment}-cluster"
-}
-
-resource "aws_ecs_task_definition" "this" {
-  family                   = "${var.environment}-task"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = 256
-  memory                   = 512
-  execution_role_arn       = var.execution_role_arn
-
-  container_definitions = jsonencode([
-    {
-      name  = "react-app"
-      image = var.image_uri
-
-      portMappings = [
-        {
-          containerPort = 80
-        }
-      ]
-    }
-  ])
-}
-
 resource "aws_ecs_service" "this" {
   name            = "${var.environment}-service"
   cluster         = aws_ecs_cluster.this.id
